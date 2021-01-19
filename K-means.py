322509d48a85a87d474a983ead3caa8b043d4d68
Piyushagg19 <64212635+Piyushagg19@users.noreply.github.com>
2021-01-19 23:18:08 +0530

    Update K-means.py

--- K-means.py
+++ K-means.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from os import path
 
-MODEL_FILE = 'trained_kmeans.pkl'
+MODEL_FILE = 'trained_Kmeans.pkl'
 LL_FILE = 'trained_ll.pkl'
 SS_FILE = 'trained_ss.pkl'
 
